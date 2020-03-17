6276997316eb6365d5ee500f7a7197a1ebba3432
Jonathan <joncomas@outlook.com>
2020-03-17 02:06:10 +0000

    working

--- api/models.py
+++ api/models.py
@@ -4,23 +4,23 @@ All your application modules and serializers are going to be declared inside thi
 from rest_framework import serializers
 from django.db import models
 
-"""
-Define he Contact Entity into your applcation model
-"""
-class Contact(models.Model):
-    first_name = models.CharField(max_length=50, default='')
-    last_name = models.CharField(max_length=50, default='')
-    phone_number = models.CharField(max_length=15, default='')
-    email = models.CharField(max_length=150, default='')
 
 """
-The ContactSerializer is where you will specify what properties
-from the ever Contact should be inscuded in the JSON response
+Define he User Entity into your applcation model
 """
-class ContactSerializer(serializers.ModelSerializer):
+class User(models.Model):
+
+    password = models.CharField(max_length=150, blank=False)
+    email = models.CharField(max_length=150, blank=False)
+
+"""
+The UserSerializer is where you will specify what properties
+from the ever User should be inscuded in the JSON response
+"""
+class UserSerializer(serializers.ModelSerializer):
 
 
     class Meta:
-        model = Contact
+        model = User
         # what fields to include?
-        fields = ('first_name','last_name', 'phone_number', 'email')
+        fields = ('id', 'password', 'email')
--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from api import views
+
 urlpatterns = [
-    path('contacts/<int:contact_id>', views.ContactsView.as_view(), name='id-contacts'),
-    path('contacts/', views.ContactsView.as_view(), name='all-contacts'),
+    path('users/<int:user_id>', views.UsersView.as_view(), name='id-users'),
+    path('users/', views.UsersView.as_view(), name='all-users')
 ]
